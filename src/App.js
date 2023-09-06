075a5c95ae0f871a99344b1940eea728da82d43d
AcaciaVA <acacia.lord@va.gov>
2023-09-06 12:11:02 -0400

    functions & parameters

--- src/App.js
+++ src/App.js
@@ -55,3 +55,64 @@ console.log(10 === 5);
 //   console.log("works");
 // }
 
+// function
+// or () => which is called an 'arrow function syntax' can be used as 
+// functions
+
+//Defining code that is not executed immediately, but instead later 
+//when the function is called - like a Java method - and as many times 
+//as you want
+function greet(){
+  console.log('Hello from function call');
+}
+
+//Calling execution of greet function
+greet();
+
+//Function with paramaters (i.e. input values)
+function greetwithparameters(username, message){
+  console.log(username);
+  console.log(message);
+}
+
+//Calling execution of greetwithparameters function
+greetwithparameters("Max", "Hi there! ");
+
+//Function with paramaters (i.e. input values), that have default values
+function greetwithDefaultparameters(username, message = "Hello"){
+  console.log(username);
+  console.log(message);
+}
+
+//Calling execution of greetwithDefaultparameters function, you can 
+//also override the default value by inserting the message value like 
+//the above example - as if the default value does not exist
+greetwithDefaultparameters("Freddi");
+
+//Function with paramaters (i.e. input values), that have default values
+//and a return statement
+//Functions must only have one return statement at most. Functions w/out
+//"return" implicitly return "undefined"
+function greetwithReturnValues(username, message = "I wanted to say Hello!"){
+  //console.log(username);
+  //console.log(message);
+  //Since we are not printing to the console, like the above 2 lines, 
+  //we should store the return value, in the line below, in either a 
+  //constant or a variable - shown on line 112 OR we can call the 
+  //function itself on the console as shown on line 108
+  return "Hi, I am " + username + ". " + message;
+}
+
+//Calling execution of greetwithReturnValues function, within a 
+//console log call
+console.log(greetwithReturnValues("Mark"));
+
+//const value containing the value of function greetwithReturnValues 
+//after execution
+const greeting = greetwithReturnValues("Markus");
+
+//const value is printed to the screen
+console.log(greeting);
+
+
+
