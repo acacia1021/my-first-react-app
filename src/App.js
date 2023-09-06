dd5717ffffa1c496217e0652dea7eda0a3cd576d
AcaciaVA <acacia.lord@va.gov>
2023-09-06 10:42:01 -0400

    Variables & Constants

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+//The below is notes compiled from watching JavaScript Videos - it does not reflect the REACT project listed above
+
+//Hello World will show in the console
+console.log('Hello World');
+
+//Variable holding value
+let userMessage = "Hello World!";
+//Console statemnet to print variable value
+console.log(userMessage);
+
+//Constants - constants cannpt be reassigned, use a variable to reassign
+const userMessage2 = "Hello World2";
+
+console.log(userMessage2);
+
